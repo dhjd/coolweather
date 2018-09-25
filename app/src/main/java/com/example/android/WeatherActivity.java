8a942096d643caee0033db9a23330382dd0872ba
dhjd <2508691426@qq.com>
2018-09-25 16:46:07 +0800

    增加后台自动更新天气的功能。

--- app/src/main/java/com/example/android/WeatherActivity.java
+++ app/src/main/java/com/example/android/WeatherActivity.java
@@ -1,6 +1,7 @@
 package com.example.android;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -23,6 +24,7 @@ import com.bumptech.glide.Glide;
 import com.example.android.db.Province;
 import com.example.android.gson.Forecast;
 import com.example.android.gson.Weather;
+import com.example.android.service.AutoUpdateService;
 import com.example.android.util.HttpUtil;
 import com.example.android.util.Utility;
 
@@ -238,5 +240,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
